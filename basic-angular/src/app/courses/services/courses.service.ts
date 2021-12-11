7d6ace547ff548530443951c507c69ab264b5e63
jian-kowalski <jian_kowalski@hotmail.com>
2021-12-11 13:49:44 -0300

    Criação do projeto

--- basic-angular/src/app/courses/services/courses.service.ts
+++ basic-angular/src/app/courses/services/courses.service.ts
@@ -1,17 +1,22 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { first } from 'rxjs';
+import { environment } from './../../../environments/environment';
 import { Course } from './../model/course';
-import { delay, first } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CoursesService {
+  private getHostURL(): string {
+    console.log(environment.apiCourses);
+    return  "http://localhost:8080/courses";
+  }
   private readonly API = '/assets/courses.json';
 
   constructor(private httpClient: HttpClient) {}
 
   list() {
-    return this.httpClient.get<Course[]>(this.API).pipe(first());
+    return this.httpClient.get<Course[]>(this.getHostURL()).pipe(first());
   }
 }
